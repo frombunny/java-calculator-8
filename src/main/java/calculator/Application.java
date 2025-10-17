5e6a1e519565959b364c4496366817e487f7d5ba
frombunny <rucy011121@gmail.com>
2025-10-17 14:55:22 +0900

    feat(Application): Application에 동작 코드 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.domain.CustomDelimiterHandler;
+import calculator.domain.InputParser;
+import calculator.domain.InputValidator;
+import calculator.domain.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        InputValidator inputValidator = new InputValidator();
+        CustomDelimiterHandler customDelimiterHandler = new CustomDelimiterHandler();
+        InputParser inputParser = new InputParser(customDelimiterHandler);
+        Calculator calculator = new Calculator();
+
+        StringCalculator stringCalculator = new StringCalculator(inputView, inputValidator, inputParser, calculator,
+                outputView);
+
+        stringCalculator.run();
     }
 }
